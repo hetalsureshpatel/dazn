96108c7ce003dd58a057aae084978b40b19f877f
Hetal Patel <hetal.suresh.patel@gmail.com>
2022-07-17 14:15:30 +0100

    Set out the structe of the appliaction

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
 import logo from '../../logo.svg';
 import './app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <h1>
+          DAZN Coding Challenge
+        </h1>
+        <p>Using the Movie DB to create search of movies</p>
       </header>
+      <div>
+        Search
+      </div>
     </div>
   );
 }
