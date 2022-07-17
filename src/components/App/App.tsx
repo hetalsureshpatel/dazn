551509c3d297aa01ce8f8513678268455f6bcd94
Hetal Patel <hetal.suresh.patel@gmail.com>
2022-07-17 15:57:39 +0100

    Prettier was run

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,16 @@
+import SearchMovies from '../SearchMovies';
+
 import './app.scss';
 
 function App() {
   return (
     <div className="app">
       <header className="header">
-        <h1>
-          DAZN Coding Challenge
-        </h1>
+        <h1>DAZN Coding Challenge</h1>
         <p>Using the Movie DB to create search of movies</p>
       </header>
       <div>
-        Search
+        <SearchMovies />
       </div>
     </div>
   );
